74894b07f57ca685cb3f2286dc5f33e13112de75
allezz <aerbay@gmail.com>
2020-05-27 18:14:48 +0200

    Add serializers for image, Increase premium snippet characters

--- app/serializers/article/show_serializer.rb
+++ app/serializers/article/show_serializer.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 
 class Article::ShowSerializer < ActiveModel::Serializer
-
+  include Rails.application.routes.url_helpers
   attributes :id, :title, :body, :published_at, :premium, :image
-  
+
   def published_at
     object.created_at.strftime('%F %R')
   end
 
   def body
-    object.premium && current_user.nil? ? object.body[0..99] : object.body
+    object.premium && current_user.nil? ? object.body[0..299] : object.body
   end
+
+  def image
+    return nil unless object.image.attached?
+
+    if Rails.env.test?
+      rails_blob_url(object.image)
+    else
+      object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+    end
+end
 end
