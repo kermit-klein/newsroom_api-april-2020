24a73997bd99ca8de193767212f5bb6a63bd4f4d
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-28 17:43:58 +0200

    making controller as oli wants it...

--- app/controllers/api/admin/articles_controller.rb
+++ app/controllers/api/admin/articles_controller.rb
@@ -2,26 +2,18 @@
 
 class Api::Admin::ArticlesController < ApplicationController
   before_action :authenticate_user!
+  before_action :editor?
 
   def index
-    if check_authorized(:editor)
       article = Article.where(published: false)
       render json: article, each_serializer: Admin::Article::IndexSerializer
-    end
   end
-end
 
-private
+  private
 
-def render_unauthorized
-  render json: { message: 'You are not authorized', errors: ['You are not authorized'] }, status: 401
-end
-
-def check_authorized(limit)
-  unless User.roles[current_user.role] >= User.roles[limit]
-    render_unauthorized()
-    return false
-  else
-    return true
+  def editor?
+    unless current_user.role == 'editor'
+      render json: { message: 'You are not authorized', errors: ['You are not authorized'] }, status: 401
+    end
   end
 end
