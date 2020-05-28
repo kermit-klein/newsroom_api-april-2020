1f5f79bf481c4a5ce8cba4c8f1b8f8c21d4b0458
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-28 16:36:11 +0200

    making the code good

--- app/controllers/api/admin/articles_controller.rb
+++ app/controllers/api/admin/articles_controller.rb
@@ -2,26 +2,13 @@
 
 class Api::Admin::ArticlesController < ApplicationController
   before_action :authenticate_user!
-  before_action { check_user_role() }
 
   def index
-    if current_user.role == 'editor'
+    if current_user.role >= 'editor'
       article = Article.where(published: false)
       render json: article, each_serializer: Admin::Article::IndexSerializer
     else
       render_unauthorized
     end
   end
-end
-
-private
-
-def check_user_role()
-  if current_user.role == 'user' || current_user.role == 'subscriber'
-    render_unauthorized
-  end
-end
-
-def render_unauthorized()
-  render json: { message: 'You are not authorized', errors: ['You are not authorized'] }, status: 401
 end
