bbeaee0bd1f9f0811b7312e93c393243400be0e7
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-01 13:24:00 +0200

    adding  to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,11 @@
   categories = ["other", "sport", "local", "politics", "economy", "world", "entertainment"]
   premium = [true, false, false, false]
   article = Article.new(
-    title: Faker::Company.bs.capitalize, body: Faker::Lorem.paragraph(sentence_count: 70), category: categories.sample, premium: premium.sample
+    title: Faker::Company.bs.capitalize, 
+    body: Faker::Lorem.paragraph(sentence_count: 70), 
+    category: categories.sample, premium: premium.sample, 
+    published: true, 
+    published_at: Time.now
   )
   article.image.attach(io: file, filename: 'image.jpg')
   article.save
