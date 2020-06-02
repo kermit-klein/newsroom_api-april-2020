4cf07974fbdfb8578d492b50ff832f0b246a3757
japh <jenny.scherr@svenskaspel.se>
2020-06-02 10:07:18 +0200

    removed unused code

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,15 +4,5 @@ class Article < ApplicationRecord
   has_one_attached :image
   validates_presence_of :title, :body, :category, :location
   enum category: %i[other sport local politics economy world entertainment]
-  # validates :location, presence: { accept: 'Sweden', message: "Should have a valid location" }
-  # validate :locations, on: :update
-
-  # def locations 
-  #   if location != "Sweden"
-  #     errors.add(:location, "Should have a valid location")
-  #     # binding.pry
-  #     # errors.add(:message, "Should have a valid location")
-  #   end
-  # end
 end
 
